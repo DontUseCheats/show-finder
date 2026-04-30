8e9deea9d6d45af54350e42bfbe1cd244d7d4f83
justinkimm17 <183580285+DontUseCheats@users.noreply.github.com>
2026-04-29 19:08:17 -0700

    Created main page and login page html and css

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/auth/spotify")
+def auth_spotify():
+    return redirect(spotify_auth_url)
